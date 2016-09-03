2f5195a98d18b3d4e5af6b7ff8c53611d53219e4
Vincent Nguyen <vnguyen2@gmail.com>
2016-09-02 22:52:50 -0500

    logic for right or wrong and updated stats

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,26 @@
-//variable object with questions, answers, and the index of correct answer
+$(function() {
+//variable object with questions, answers, and the correct answer reflects index position on answer array
 
 var triviaQuestions = [{
 	question: "In which country is the bay of pigs?",
 	answers: ["Greece", "Cuba", "Vietnam", "United States of America"],
-	correct: 2
+	correct: 1
 }, {
 	question: "Of the ten largest islands in the world, three are in which country?",
 	answers: ["Canada", "Brazil", "Phillipines", "Indonesia"],
-	correct: 1
+	correct: 0
 }, {
 	question: "What is the largest sea body in the world, excluding the oceans?",
 	answers: ["Mediterranian Sea", "The Caribbean Sea", "The Sea of Okhotsk", "The South China Sea"],
-	correct: 4
+	correct: 3
 }, {
 	question: "What is the largest country in the world without permanent rivers?",
 	answers: ["Iraq", "Pakistan", "Saudi Arabia", "Iran"],
-	correct: 3
+	correct: 2
 }, {
 	question: "The river Rhine flows through eight countries, which one of the following does it NOT flow through?",
 	answers: ["Germany", "Luxembourg", "Poland", "Italy"],
-	correct: 3
+	correct: 2
 }, {
 	question: "What is the official nickname of the state of Texas?",
 	answers: ["The Friendship State", "The Chili State", "The Home of Tex-Mex State", "The Lone Star State"],
@@ -27,16 +28,17 @@ var triviaQuestions = [{
 }, {
 	question: "What is the highest mountain in the world?",
 	answers: ["Mt Everest", "K2", "Cho Oyu", "Broad Peak"],
-	correct: 1
+	correct: 0
 }, {
 	question: "Which is the longest river in the world?,",
 	answers: ["Mississippi River", "Nile River", "Amazon River", "Yangtze River"],
-	correct: 2
+	correct: 1
 }, {
 	question: "What is the smallest nation in Africa?",
 	answers: ["Cape Verde", "Rwanda", "Burundi", "The Gambia"],
-	correct: 4
+	correct: 3
 }];
+
 
 //variables for recording values during game, timer, and game ending 
 var questionIndex = 0,
@@ -51,37 +53,68 @@ var timer = {
 	startTimer: function(start) {
 		timeLeft = start;
 		$('#timer').html('<h2>' + timeLeft + '</h2>');
-		intervalID = setInterval(timerCount, 1000);
+		intervalID = setInterval(this.timerCount, 1000);
 	},	
+
+	timerReset: function() {
+		clearInterval(intervalID);
+	},
 
 	timerCount: function() {
 		if(timeLeft == 0) {
-			timerReset();
+			timer.timerReset();
 		} else {
 			timeLeft--
 			$('#timer').html('<h2>' + timeLeft + '</h2>');
 		}
 	},
-	//stop timer
-	timerReset: function() {
-		clearInterval(intervalID);
-	},
 };
 
 var question = {
+
+	//display question and answers
 	displayQuestion: function() {
-		q = triviaQuestions[questionIndex]
-		$('#question').html(q.question);
+		 q = triviaQuestions[questionIndex];
+			$('#question').html(q.question);
 		for (var i = 0; i < q.answers.length; i++) {
-				$('.buttonValue' + i).html(q.answers[i]); 
+				$('.btnVal' + i).html(q.answers[i]); 
 				console.log(q.answers[i])
-			};
+			}
+			timer.startTimer(2);
 	},
+	//check answers for right or wrong
+	checkAnswers: function(answer) {
+		timer.timerReset();
+		var rightAnswer = q.correct;
+		if (q.correct == answer) {
+			$('#messages').html('You are CORRECT!!!!');
+				correctAns++;
+		} else {
+			$('#messages').html('You will never catch me. The right answer is ' + q.answers[rightAnswer]);
+				incorrectAns++;
+		  }
+	}
+	//goes to next question after times runs out or if right or wrong asnwer picked
+
 };
+
+//click action for answers
+	$('.btnVal0').click(function() {
+		question.checkAnswers(0);
+	}); 
+	$('.btnVal1').click(function() {
+		question.checkAnswers(1);
+	}); 
+	$('.btnVal2').click(function() {
+		question.checkAnswers(2);
+	});
+	$('.btnVal2').click(function() {
+		question.checkAnswers(3);
+	});
 
 
 question.displayQuestion();
-
+});
 
 
 
